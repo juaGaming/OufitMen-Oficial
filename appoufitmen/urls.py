6866d3a641b011af5998f33e0158847c60686316
juaGaming <153557310+juaGaming@users.noreply.github.com>
2024-05-28 18:15:27 -0500

    configuracion de las urls

--- appoufitmen/urls.py
+++ appoufitmen/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from appoufitmen import views
+from . views import*
 from .viewsLogin import*
 from .import viewsLogin
 
 urlpatterns = [
+    
+    #Vistas Administrador
+    path('InventarioAdmin',views.InventarioAdmin,name="InventarioAdmin"),
+    path('InsertarPrendas/',InsertarProducto.as_view(),name='InsertarPrendas'),
+    path('listarPrendas',ListarPrendas.as_view(), name='listarPrendas'),
+    path('ActualizarProducto/<int:pk>',ActualizarProducto.as_view(), name='ActualizarProducto'),
+    path('BuscarProducto/<int:pk>/', views.BuscarProducto.as_view(), name='BuscarProducto'),
+    path('EliminarPrendas/<int:pk>',EliminarPrendas.as_view(),name='eliminar'),
+    
+    #Vistas Libres
     path('', views.inicio, name="inicio"),
     path('Casual/',views.Casual, name="RegistroUsuario"),
     path('Formal/',views.Formal, name="RegistroUsuario"),
