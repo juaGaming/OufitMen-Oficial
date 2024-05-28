8712c76e4a603bc84877cb820bd0efc29de6f046
juaGaming <153557310+juaGaming@users.noreply.github.com>
2024-05-28 17:51:55 -0500

    configuracion de urls y views

--- appoufitmen/urls.py
+++ appoufitmen/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from appoufitmen import views
 urlpatterns = [
-    path('', views.inicio, name="inicio")
+    path('', views.inicio, name="inicio"),
+    path('Casual/',views.Casual, name="RegistroUsuario"),
+    path('Formal/',views.Formal, name="RegistroUsuario"),
 ]
--- appoufitmen/views.py
+++ appoufitmen/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
+
+def Casual(request):
+    return render (request, "Casual.html")
+
+def Formal(request):
+    return render (request, "Formal.html")
