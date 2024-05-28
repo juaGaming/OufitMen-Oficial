3292f4f2d87798e6d3a14a41ebeebda6243b3aa4
juaGaming <153557310+juaGaming@users.noreply.github.com>
2024-05-28 18:04:43 -0500

    vista login y vista registro

--- appoufitmen/urls.py
+++ appoufitmen/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from appoufitmen import views
+from .viewsLogin import*
+from .import viewsLogin
+
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('Casual/',views.Casual, name="RegistroUsuario"),
     path('Formal/',views.Formal, name="RegistroUsuario"),
+    
+    #Autenticacion
+    path('Iniciar_Sesion/',viewsLogin.Iniciar_Sesion, name="Iniciar_Sesion"),
+    path('RegistroUsuario/',viewsLogin.RegistrarUsuarios, name="RegistroUsuario"),
+    path('Cerrar_Sesion/',viewsLogin.Cerrar_Sesion, name="RegistroUsuario"),
 ]
