f2aac2a9ed40a9c7cb7b0a1926e8861cec3cb537
juaGaming <153557310+juaGaming@users.noreply.github.com>
2024-05-27 23:34:39 -0500

    configuracion de archivos estaticos

--- outfitmen/settings.py
+++ outfitmen/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'appoufitmen/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
