c258a74b7f64e11e0b39f2f6db952324783427c4
juaGaming <153557310+juaGaming@users.noreply.github.com>
2024-05-27 23:55:54 -0500

    configuracion conexion base de datos

--- outfitmen/settings.py
+++ outfitmen/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 import os
 
+import dj_database_url
+
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +84,7 @@ WSGI_APPLICATION = 'outfitmen.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
